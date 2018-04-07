d8a60398b792b4dc6b13e3078ca2730c0077c064
LoliMay <1404363070@qq.com>
2018-04-08 00:31:38 +0800

    以更优雅的方式重构源代码

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,19 @@
 Page({
   data: {
-    foreColor:'#2f7ff0'
+    stuAddress:'未知区域',
+    time1:'00:00:00',
+    time2:'00:00:00',
+    buttonValue:'开始签到',
+    addressColor: '',
+    messageColor: '',
+    buttonBgColor: '#2f7ff0'
   },
+  tapButton: function(){
+    var buttonValue = this.data.buttonValue === '开始签到'?'结束自习':'开始签到'
+    var buttonBgColor = this.data.buttonBgColor === '#2f7ff0' ? '#cc4125' : '#2f7ff0'
+    this.setData({
+      buttonValue: buttonValue,
+      buttonBgColor: buttonBgColor
+    })
+  }
 })
